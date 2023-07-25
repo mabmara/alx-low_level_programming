9b7c7edef773f06829abb77e7300adba8582bc1b
mabmara <mabmaraorina@gmail.com>
2023-07-25 14:35:22 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - will print a string in reverse
+ * @s: it is the string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+        int i = 0;
+
+        while (s[i] != '\0')
+        {
+                i++;
+        }
+        i = i - 1;
+        while (i >= 0)
+        {
+                _putchar(s[i]);
+                i--;
+        }
+        _putchar('\n');
+}
+
