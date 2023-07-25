b5102c8fb1364aedac647ccf95564ee94cef526e
mabmara <mabmaraorina@gmail.com>
2023-07-25 14:37:09 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int j = 0, i = 0;
+	char str[500];
+
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		s[i] = str[j];
+		j++;
+		i--;
+	}
+}
+
